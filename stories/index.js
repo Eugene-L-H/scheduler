bdc513b9c4bb9268dad45f965a0914df716e6915
Eugene <eugenehelland@mail.com>
2022-04-21 10:43:05 -0600

    Create Status and Confirm components, write corresponding storybook tests.

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,8 @@ import Appointment from 'components/Appointment/index';
 import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
+import Confirm from 'components/Appointment/Confirm';
+import Status from 'components/Appointment/Status';
 
 storiesOf('Button', module)
   .addParameters({
@@ -150,5 +152,12 @@ storiesOf('Appointment', module)
     student={'Lydia Miller-Jones'} 
     interviewer={interviewers}
     onEdit={action('onEdit')}
-    onDelete={action('onDelete')} />);
+    onDelete={action('onDelete')} 
+  />)
+  .add('Confirm', () => <Confirm 
+    message={'Delete the Appointment?'}
+    onConfirm={action('onConfirm')}
+    onCancel={action('onCancel')} 
+  />)
+  .add('Status', () => <Status message={'Deleting'}/>);
 
