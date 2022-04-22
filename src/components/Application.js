f5328288140a2587c269195859d188d441448888
Eugene <eugenehelland@mail.com>
2022-04-22 14:01:18 -0600

    Delete hard coded days data, useEffect hook with axios request to /api/days used instead.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,27 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import 'components/Application.scss';
 import DayList from './DayList';
 import InterviewerList from './InterviewerList';
 import Appointment from './Appointment';
-
-const days = [
-  {
-    id: 1,
-    name: 'Monday',
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: 'Tuesday',
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: 'Wednesday',
-    spots: 0,
-  },
-];
 
 const appointments = {
   '1': {
@@ -64,7 +47,13 @@ const appointments = {
 
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState('Monday');
+
+  useEffect(() => {
+    axios.get('http://localhost:8001/api/days')
+      .then(setDays((response) => response));
+  },[]);
 
   const appointmentsArr = Object.values(appointments).map((appointment) => {
     if (appointment.interview){
