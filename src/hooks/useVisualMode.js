ee9d3ac110648b077d4dae1133a14a2cbaaaca4b
Eugene <eugenehelland@mail.com>
2022-04-23 14:28:30 -0600

    transition and back functions implemented in useVisualMode.js and passing tests.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -5,13 +5,16 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
   const transition = function(newMode, replace = false){
-    if (!replace){
-      history.push(newMode);
-    }
+    if (!replace) history.push(newMode);
     setMode(newMode);
   };
 
+  const back = function () {
+    if (history.length === 1) return;
+    // Remove current mode setting, set mode to last item in history array
+    history.pop();
+    setMode(history[history.length - 1]);
+  }
 
-
-  return { mode, transition }
+  return { mode, transition, back }
 };
